6a23fb7068b93aabab5b4bf1c9a0775f7a128878
Kaue Francisco <kaue_francisco70@hotmail.com>
2025-03-23 15:21:38 -0300

    :bug: fix: Fix and make hex to oklch conversion function functional.

--- src/convert.py
+++ src/convert.py
@@ -1,23 +1,32 @@
-from colorsys import rgb_to_hsv
+import math
 
 
-def hex_to_oklch(hex):
+def hex_to_oklch(hex_color):
+    hex_color = hex_color.lstrip('#')
 
-    def hex_to_oklch(hex_color):
-        r, g, b = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-        r, g, b = r / 255.0, g / 255.0, b / 255.0
+    r, g, b = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    r, g, b = r / 255.0, g / 255.0, b / 255.0
 
-        r = r**2.2
-        g = g**2.2
-        b = b**2.2
+    gama = 2.16
 
-        l = (0.2126*r + 0.7152*g+0.0722*b) ** (1/2.2)
-        a = r-g
-        b = b-g
+    r = r ** gama
+    g = g ** gama
+    b = b ** gama
 
-        chroma = (a**2 + b**2) ** 0.5
-        hue = (a / chroma if chroma != 0 else 0)
+    l = (0.4122214708 * r + 0.5363325363 * g + 0.0514459929 * b) ** (1/3)
+    m = (0.2119034982 * r + 0.6806995451 * g + 0.1073969566 * b) ** (1/3)
+    s = (0.0883024619 * r + 0.2817188376 * g + 0.6299787005 * b) ** (1/3)
 
-        return (l, chroma, hue)
+    L = 0.2104542553 * l + 0.7936177850 * m - 0.0040720468 * s
+    a = 1.9779984951 * l - 2.4285922050 * m + 0.4505937099 * s
+    b = 0.0259040371 * l + 0.7827717662 * m - 0.8086757660 * s
 
-    hex_to_oklch(hex)
+    chroma = math.sqrt(a ** 2 + b ** 2)
+
+    hue = math.degrees(math.atan2(b, a))
+    if hue < 0:
+        hue += 360
+
+    L = L * 100
+
+    return (f"oklch({L:.1f}% {chroma:.3f} {hue:.3f})")
--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 import flet as ft
+import convert as convert
 
 
 def main(page: ft.Page):
     page.title = "Convert hex to oklch"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    page.window.max_height = 500
-    page.window.max_width = 450
+    page.window.height = 500
+    page.window.width = 450
     page.window.min_height = 400
     page.window.min_width = 400
-    page.window.resizable = False
+    page.window.center()
+    page.bgcolor = ('#191615')
 
     title = ft.Text(
         value="Converter HEX em OKLCH",
@@ -19,42 +21,34 @@ def main(page: ft.Page):
     )
 
     def copy_oklch(e):
-        page.set_clipboard(cod_oklch)
+        page.set_clipboard(oklch.value)
         page.open(ft.SnackBar(ft.Text("OKLCH Copiado!")))
         page.update()
 
-    def RGB_RGBA(e):
-        hex.value = ""
-        hex.max_length = int(e.control.value)
+    def convert_color(e):
+        oklch.value = convert.hex_to_oklch(hex.value)
         page.update()
 
     hex = ft.TextField(label="HEX", prefix_text="#",
                        capitalization=ft.TextCapitalization.CHARACTERS, max_length=6)
 
-    rgb_rgba = ft.RadioGroup(content=ft.Row([
-        ft.Radio(value="6", label="RGB"),
-        ft.Radio(value="8", label="RGBA")]), on_change=RGB_RGBA, value="6")
-
-    cod_oklch = f"oklch(59% 0.145 163.225)"
-    # cod_ = f"oklch({l}% {c} {m})"
-
     oklch = ft.TextField(label="OKLCH", read_only=True,
-                         value=cod_oklch, suffix_icon=ft.IconButton(ft.icons.CONTENT_COPY, on_click=copy_oklch))
+                         value="", suffix_icon=ft.IconButton(ft.icons.CONTENT_COPY, on_click=copy_oklch))
 
     btn = ft.OutlinedButton("Converter Cor",
                             style=ft.ButtonStyle(
-                                color=ft.colors.GREEN_500,
+                                color=('#7acc00'),
                                 text_style=ft.TextStyle(
                                     size=18,
                                     weight=ft.FontWeight.BOLD
                                 ),
-                                shape=ft.RoundedRectangleBorder(radius=5)
-                            ))
+                                shape=ft.RoundedRectangleBorder(radius=5),
+                                side=ft.BorderSide(width=2, color='#7acc00')),
+                            on_click=convert_color)
 
     page.add(
         ft.Row(controls=[title], alignment=ft.MainAxisAlignment.CENTER),
         ft.Row(controls=[hex], alignment=ft.MainAxisAlignment.CENTER),
-        ft.Row(controls=[rgb_rgba], alignment=ft.MainAxisAlignment.CENTER),
         ft.Row(controls=[oklch], alignment=ft.MainAxisAlignment.CENTER),
         ft.Row(controls=[btn], alignment=ft.MainAxisAlignment.CENTER),
     )
